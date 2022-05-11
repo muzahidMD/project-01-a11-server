3a41acfcd354bc22a7c15b5037e1858c79806fb2
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-11 23:07:53 +0600

    inclued post api

--- index.js
+++ index.js
@@ -30,7 +30,14 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
-        })
+        });
+
+        // POST
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
     }
     finally { }
 }
